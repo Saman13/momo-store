86c0bb02501187d8688f5d68a14fc056c9cfd41b
Saman <samanpunk@yandex.ru>
2024-11-21 12:08:14 +0300

    add terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,8 +4,8 @@ terraform {
   bucket = "terraform-state-saman"
   region = "ru-central1"
   key = "bucket/terraform.tfstate"
-  access_key = "aje1j5uq9jucjduuntoh"
-  secret_key = "AQVNwZl9pM_Mm33r9xG5nleFAWuJ76LXoGNfmDED"
+  access_key = "YCAJELbbZRJ5cRfJG7gq_Oxfo"
+  secret_key = "YCPRUhzwQQke1gbsz3TV6EYxQYDG2y5OKMgJTYPm"
   skip_region_validation = true
   skip_credentials_validation = true
   }
